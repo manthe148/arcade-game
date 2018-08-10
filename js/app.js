ae575bf58905849e27a89d084bc8974a520b41d2
Matthew McDonald <matt.mcodnald21@gmail.com>
2018-08-10 14:31:55 -0500

    first commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 // Enemies our player must avoid
+
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -29,8 +30,10 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+const allEnemies =[];
+const player = {
+    player: 'user'
+} 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
